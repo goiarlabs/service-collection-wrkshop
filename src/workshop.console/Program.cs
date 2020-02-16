f7e464a7e65613d49f58da783929c02db726d097
lazarogabriel <lazarogabriel2000@gmail.com>
2020-02-15 22:13:08 -0300

    Add solution one (probably very bad)

--- src/workshop.console/Program.cs
+++ src/workshop.console/Program.cs
@@ -11,43 +11,86 @@ namespace workshop.console
 
         static void Main(string[] args)
         {
-            ConfigureServices();
+            // No se con exactitud en donde se tiene que resolver dinamicamente el ServiceCollection
+            // el idioma con el que tiene que saludar un usuario o si lo tiene que resolver obligatoriamente el ServiceCollection
+            var user = SetUserInformation();
+
+            ConfigureServices(user);
 
             using (var scope = serviceProvider.CreateScope())
             {
-                DoWork(scope.ServiceProvider);
+                DoWork(scope.ServiceProvider, user);
             }
         }
 
-        private static void DoWork(IServiceProvider serviceProvider)
+        private static void DoWork(IServiceProvider serviceProvider, Usuario user)
         {
             var controller = new GreeterController(serviceProvider.GetService<IGreetService>());
 
-            controller.Greet("Pepe");
+            controller.Greet(user.nombre);
         }
 
-        private static void ConfigureServices()
+        private static void ConfigureServices(Usuario user)
         {
             IServiceCollection services = new ServiceCollection();
 
-            services.AddGreeter(a => a.UseSpagnolo());
-
+            // La forma de resolverlo
+            switch (user.idioma)
+            {
+                case "Español":
+                    services.AddGreeter(a => a.UseSpagnolo());
+                    break;
+                default:
+                    services.AddGreeter(a => a.UseDefault());
+                    break;
+            }
+            
             serviceProvider = services.BuildServiceProvider();
         }
-    }
 
-    public class GreeterController
-    {
-        private readonly IGreetService _greetService;
 
-        public GreeterController(IGreetService greetService)
+        // La forma de levantar informacion de un usuario
+        public static Usuario SetUserInformation()
         {
-            _greetService = greetService;
+            Console.WriteLine("Ingresa tu nombre: ");
+            var nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu idioma: ");
+            var idioma = Console.ReadLine();
+
+            return new Usuario(nombre, idioma);
         }
 
-        public void Greet(string name)
-        {
-            Console.WriteLine(_greetService.Greet(name));
-        }
     }
 }
+
+public class GreeterController
+{
+    private readonly IGreetService _greetService;
+
+    public GreeterController(IGreetService greetService)
+    {
+        _greetService = greetService;
+    }
+
+    public void Greet(string name)
+    {
+        Console.WriteLine(_greetService.Greet(name));
+    }
+}
+
+
+// Un objecto usuario para transportar la informacion
+public class Usuario
+{
+    public string nombre;
+    public string idioma;
+
+    public Usuario(string nombre, string idioma)
+    {
+        this.nombre = nombre;
+        this.idioma = idioma;
+    }
+}
+
+
